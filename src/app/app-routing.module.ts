84a29810fb31d157a53c92e9d27b41dd1d39d9d4
Leonardo Fadul <lsffadul@gmail.com>
2021-11-03 07:46:08 -0300

    HomePage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'home-page',
+    loadChildren: () => import('./pages/home-page/home-page.module').then( m => m.HomePagePageModule)
   }
 ];
 
